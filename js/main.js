b4276831899f2e765a1cbf2aa25852abd9ae090a
MauOnline <mtx@MTX-LAPTOP.localdomain>
2019-06-10 17:48:03 +0100

    Initiate article template and other template update

--- js/main.js
+++ js/main.js
@@ -1,4 +1,12 @@
 $(function () {
     //Init scripts
     console.log("Loaded !");
+
+    //actNow-modal
+    $('#actNow').on('change', function () {
+        // alert('Model shown');
+        if ($(this).val() > 0) {
+            $('#actNow-modal').modal('show');
+        }
+    });
 })
